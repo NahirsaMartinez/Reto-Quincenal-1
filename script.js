f8680b0d66bd20172d597918ec1c3d34cbd25a7f
Nahir Martinez <nahirmartinez09@gmail.com>
2023-04-26 19:42:17 -0300

    Segundo commit:Añadido edad,correo,dni

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-const inputInfo = document.querySelector ("#entrada-nombre");
-const pBienvenida = document.querySelector ("#msj-bienvenida");
+const inputNombre = document.querySelector("#entrada-nombre");
+const inputEdad = document.querySelector("#entrada-edad");
+const inputCorreo = document.querySelector("#entrada-correo");
+const inputDni = document.querySelector ("#entrada-dni")
+const pBienvenida = document.querySelector("#msj-bienvenida");
 
+function mostrarmsj() {
+    let nombreRecibido = inputNombre.value;
+    let edadRecibido = inputEdad.value;
+    let correoRecibido = inputCorreo.value;
+    let dniRecibido = inputDni.value;
+    pBienvenida.innerHTML = ("Hola " + nombreRecibido + "!" + "<br> Tu edad es :" + edadRecibido + "<br>" + "Tu correo es:" + correoRecibido + "<br> Tu DNI Es :" + dniRecibido);
+}
 
-function mostrarmsj(){
-    nombreRecibido = inputInfo.value;
-    pBienvenida.innerHTML = ("Bienvenido/a "+ nombreRecibido+ "!");
-
+if (edadRecibido >= 18) {
+    
+    
+} else (edadRecibido <18) {
+    
 }
