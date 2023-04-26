9ec9e039efaf98ccc8ca3ca2db806bc682780ff8
Nahir Martinez <nahirmartinez09@gmail.com>
2023-04-26 20:28:52 -0300

    tercer commit añadido: if/else para saber si el usuario es mayor o menor de edad

--- script.js
+++ script.js
@@ -2,19 +2,23 @@ const inputNombre = document.querySelector("#entrada-nombre");
 const inputEdad = document.querySelector("#entrada-edad");
 const inputCorreo = document.querySelector("#entrada-correo");
 const inputDni = document.querySelector ("#entrada-dni")
-const pBienvenida = document.querySelector("#msj-bienvenida");
+const cuerpoPagina = document.querySelector ("body");
+
+let pBienvenida = document.querySelector("#msj-bienvenida");
+
 
 function mostrarmsj() {
     let nombreRecibido = inputNombre.value;
     let edadRecibido = inputEdad.value;
     let correoRecibido = inputCorreo.value;
     let dniRecibido = inputDni.value;
+
     pBienvenida.innerHTML = ("Hola " + nombreRecibido + "!" + "<br> Tu edad es :" + edadRecibido + "<br>" + "Tu correo es:" + correoRecibido + "<br> Tu DNI Es :" + dniRecibido);
+ 
+    if (edadRecibido >= 18) {
+        pBienvenida.innerHTML += (" <br> Eres mayor de edad");
+    } else {
+        pBienvenida.innerHTML += (" <br> Eres menor de edad");
 }
 
-if (edadRecibido >= 18) {
-    
-    
-} else (edadRecibido <18) {
-    
-}
+  }
